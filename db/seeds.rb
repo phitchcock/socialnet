ae396ae25e24affda2fa7a34568436598aaa70b3
phitchcock <phitchcock@me.com>
2015-06-08 11:23:30 -0700

    added seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: 'user', email: 'user@user.com', password: 'password', password_confirmation: 'password')
+User.create(username: 'user1', email: 'user1@user.com', password: 'password', password_confirmation: 'password')
+User.create(username: 'user2', email: 'user2@user.com', password: 'password', password_confirmation: 'password')
+User.create(username: 'user3', email: 'user3@user.com', password: 'password', password_confirmation: 'password')
+User.create(username: 'user4', email: 'user4@user.com', password: 'password', password_confirmation: 'password')
