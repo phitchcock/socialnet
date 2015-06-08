f17775af35667068a666b2d41cd9a605ad86aa41
phitchcock <phitchcock@me.com>
2015-06-08 12:13:38 -0700

    added friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,8 @@ class User < ActiveRecord::Base
 
   #validates :username, presence: true
   #validates :username, uniqueness: true
+
+  def request_friendship(user_2)
+    self.friendships.create(friend: user_2)
+  end
 end
